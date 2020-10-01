03fab447e9754917ebb0abc8697ad1aae3561e29
fbarqueroz <fabianbarquero9@gmail.com>
2020-10-01 01:22:51 -0600

    cambios en el JS

--- js/script.js
+++ js/script.js
@@ -6,9 +6,7 @@
 
 //Lista de tareas(array)
 let tareas = [];
-
-let datosLocalStorage = localStorage.getItem('tareas');
-      
+const datosLocalStorage = localStorage.getItem('tareas');
 if (datosLocalStorage !== null){
   tareas = JSON.parse(datosLocalStorage);
 }
@@ -27,23 +25,19 @@ function addTask(nameTarea,dateTarea,completTarea) {
     complet: completTarea,
     fecha: dateTarea,
   };
-
   // Agrega al array existente
   tareas.push(homework);
-
-  //Contador de tareas  
+  //Count  
   count++;
-
+  //Formulario para añadir tareas
   appendTaskDom(homework);
-  // Transformar en string usando Json
   localStorage.setItem('tareas', JSON.stringify(tareas));
-
 }
 
 const list = document.getElementById('task-list');
 
 function appendTaskDom(tarea) {
-
+  // Crear nueva tarea en DOM
   // Item de la lista
   const item = document.createElement('li')
   item.className = 'task-list__item';
@@ -51,36 +45,33 @@ function appendTaskDom(tarea) {
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
   checkbox.setAttribute('id', `tarea-${tarea.id}`);
-
   //Label
   const label = document.createElement('label');
   label.setAttribute('for', `tarea-${tarea.id}`);
   count++
-  label.innerHTML = `${tarea.nameTarea}-${tarea.dateTarea}`;
-
+  label.innerHTML = `${tarea.nameTarea} - ${tarea.dateTarea}`;
   // Se agregan elementos
   item.appendChild(checkbox);
   item.appendChild(label);
   list.appendChild(item);
-      
 }
 
 for(let i = 0; i < tareas.length; i++){
-  appendTaskDom(tareas)
+  appendTaskDom(tareas[i]);
 }
 
 //Controlador
 
+//Formulario para añadir tareas
 const formulario = document.getElementById('new-task-form');
 
 formulario.addEventListener('submit', (event) => {
-// Se cancela el comportamiento default del formulario.
-event.preventDefault();
-//llama a la funcion
-addTask(formulario.elements[0].value, formulario.elements[1].value, false);
-  
-// Resetear
-formulario.elements[0].value = " ";
-formulario.elements[1].value = " ";
-
+  // Se cancela el comportamiento default del formulario.
+  event.preventDefault();
+  //llama a la funcion
+  const homework = addTask(formulario.elements[0].value, formulario.elements[1].value, false);
+  appendTaskDom (homework);
+  // Resetear
+  formulario.elements[0].value = '';
+  formulario.elements[1].value = '';
 });
